5260e8d7815317012b678d751afcf46f4067ea8a
rieselgold <github@rieselgold.com>
2022-04-15 17:52:28 +0200

    Kloparty

---
+++ restroom/tilesets/rc3-mullana-neon-star.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rc3-mullana-neon-star" tilewidth="32" tileheight="32" tilecount="6" columns="6">
+ <image source="rc3-mullana-neon-star.png" width="192" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="3" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
